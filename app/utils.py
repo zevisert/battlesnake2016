e136acb24373582e2391a23c1dee1c87259536f8
Jake Runzer <jakerunzer@gmail.com>
2016-02-20 12:35:45 -0800

    add find my snake

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,7 @@
-def findMySnake()
+SNAKE_ID = 'ae68ef2a-2fc7-47a0-8b6b-cc7ae5b80d66'
+
+# returns our snake from list of snakes
+def find_my_snake(snakes):
+    for s in snakes:
+        if s.id == SNAKE_ID:
+            return s
