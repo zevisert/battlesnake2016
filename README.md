c246d950fd1d870c0529ce2ad363878c272fcb7b
Zev Isert <zev.isert@gmail.com>
2016-02-19 22:24:09 -0800

    Repo init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # battlesnake2016
-Tyson "Battle"-stella (@tysonbattistella), Jake "Run Show"-zner (@coffee-cup)  and I (@zevisert) team up to win sendwithus's battlesnake competition
+[Tyson "Battle"-stella](github.com/tysonbattistella), [Jake "Run Show"-zner](github.com/coffee-cup) and [I](github.com/zevisert) team up to win (hopefully) [sendwithus's](github.com/sendwithus) [battlesnake](http://battlesnake.io) competition.
+
+This snake can be setup and run using the instructions available at the repo containing the [skeleton code](github.com/sendwithus/battlesnake-python) for this snake AI.
---
+++ app/main.py
@@ -0,0 +1,60 @@
+import bottle
+import os
+
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.get('/')
+def index():
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+    return {
+        'color': '#00ff00',
+        'head': head_url
+    }
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'move': 'north',
+        'taunt': 'battlesnake-python!'
+    }
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+if __name__ == '__main__':
+    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
