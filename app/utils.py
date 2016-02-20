24fd86275c4b54eae4a4f356d067531a198ed82b
tysonbattistella <tyson@telmediq.com>
2016-02-20 12:40:16 -0800

    temp closest_food

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,8 @@
 def findMySnake()
+
+
+def closest_food(current_position, food_positions):
+    """
+    Given the current position, returns the closest food position
+    """
+    return food_positions[0]
